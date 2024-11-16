1a30ac89f9341d071591090c882268d09c72ff20
Bagas-Pradana <bagaspradana599@gmail.com.com>
2024-11-16 14:14:53 +0700

    menambahkan file php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote Upload</title>
+</head>
+
+<body>
+    <h5>Uji Coba</h5>
+</body>
+
+</html>
